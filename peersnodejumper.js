b1533725864b9d39a424da9b3e10f82a456d5c56
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2024-03-31 01:34:59 +0300

    Create peersnodejumper.js

---
+++ peersnodejumper.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: true, 
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    const page = await browser.newPage();
+    await page.goto('https://app.nodejumper.io/lava-testnet/sync', { waitUntil: 'networkidle0' });
+
+    await page.waitForSelector('span.token.string');
+    let data = await page.$eval('span.token.string', element => element.innerText);
+
+    // Удаляем кавычки
+    data = data.replace(/["']/g, '');
+    data = ',' + data;
+
+    // Записываем данные в файл
+    fs.appendFile('peers.txt', data + '\n', (err) => {
+        if (err) {
+            console.error('Ошибка при записи в файл:', err);
+        } else {
+            console.log('Данные успешно добавлены в файл peers.txt');
+        }
+    });
+
+    await browser.close();
+})();
